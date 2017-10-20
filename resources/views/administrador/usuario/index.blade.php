5c5dc9f4025c6b30b574b8f61847da7acd157128
omar zarate <alberich_sid@hotmail.com>
2017-10-20 11:07:17 -0500

    Se guardaron los ultimos cambios del proyecto que funciona perfectamente

--- resources/views/administrador/usuario/index.blade.php
+++ resources/views/administrador/usuario/index.blade.php
@@ -6,7 +6,7 @@
 <!-- Inicio del body del panel -->
 <div id="usuarioCRUD">
             <div class="row" >
-              <div class="col-sm-10 col-sm-offset-2">
+              <div class="col-sm-10 col-sm-offset-1">
                   <!--      inicio Wizard container        -->
                   <div class="wizard-container">
                       <div class="card wizard-card" data-color="azul" id="wizard">
@@ -19,12 +19,12 @@
                       <div name="formulario busqueda" class="form-horizontal">
 
                         <div class="row">
-                         
+
                             <div class=" col-sm-6 form-group">
 
                                 <div class="input-group col-md-8">
                                     <span class="input-group-addon" id="basic-addon1">
-                                      <i class="material-icons md-18">account_circle</i>   
+                                      <i class="material-icons md-18">account_circle</i>
                                     </span>
 
                                     <input id="username" type="username" class="form-control" name="username" value="" autofocus placeholder="Nombre de usuario" aria-describedby="basic-addon1" style="text-transform:uppercase;" v-model="searchUsuario.username">
@@ -36,12 +36,12 @@
 
 
                         <div class="row">
-                         
+
                             <div class=" col-sm-6 form-group">
 
                                 <div class=" input-group col-md-8">
                                     <span class="input-group-addon" id="basic-addon1">
-                                      <i class="material-icons md-18">person</i>   
+                                      <i class="material-icons md-18">person</i>
                                     </span>
 
                                     <input id="nombre" type="nombre" class="form-control" name="nombre" value=""  autofocus placeholder="Nombre" aria-describedby="basic-addon1" style="text-transform:uppercase;" v-model="searchUsuario.nombre" >
@@ -52,12 +52,12 @@
 
 
                         <div class="row">
-                         
+
                             <div class=" col-sm-6 form-group">
 
                                 <div class=" input-group col-md-8">
                                     <span class="input-group-addon" id="basic-addon1">
-                                      <i class="material-icons md-18">&nbsp; &nbsp;&nbsp;</i>   
+                                      <i class="material-icons md-18">&nbsp; &nbsp;&nbsp;</i>
                                     </span>
 
                                     <input id="paterno" type="paterno" class="form-control" name="paterno" value=""  autofocus placeholder="Apellido paterno" aria-describedby="basic-addon1" style="text-transform:uppercase;"  v-model="searchUsuario.paterno">
@@ -68,12 +68,12 @@
 
 
                         <div class="row">
-                         
+
                             <div class=" col-sm-6 form-group">
 
                                 <div class=" input-group col-md-8">
                                     <span class="input-group-addon" id="basic-addon1">
-                                      <i class="material-icons md-18">&nbsp; &nbsp;&nbsp;</i>   
+                                      <i class="material-icons md-18">&nbsp; &nbsp;&nbsp;</i>
                                     </span>
 
                                     <input id="materno" type="materno" class="form-control" name="materno" value=""  autofocus placeholder="Apellido materno" aria-describedby="basic-addon1" style="text-transform:uppercase;"  v-model="searchUsuario.materno" >
@@ -82,22 +82,22 @@
                             </div>
                         </div>
 
-                          
-                       
-                         
+
+
+
                         <div class="row col-sm-offset-1">
-                          
+
 
                                 <input type="submit" class="btn btn-primary" value="BUSCAR">
 
-               
+
                         </div>
-                         
 
-                               
-                        
 
-                        
+
+
+
+
 
 
                       </div><!--      FORM BUSQ      -->
@@ -118,7 +118,7 @@
                                 <th>NOMBRE</th>
                                 <th>APELLIDO PATERNO</th>
                                 <th>APELLIDO MATERNO</th>
-                    
+
                                 <th>ADTVO.</th>
                                 <th>DELEGACION</th>
                               </tr>
@@ -131,23 +131,23 @@
                                 <a href="#" class="btm btn-warning btn-sm" v-on:click.prevent="editKeep(keep)">H</a>
                                  <a href="#" class="btm btn-danger btn-sm" v-on:click.prevent="deleteKeep(keep)">V</a>
 
-                               
+
 
                                 <a href="#" class="btm btn-warning btn-sm" v-on:click.prevent="deleteKeep(keep)">E</a>
 
                                 </td>
                                 <td >@{{usuario.id}}</td>
-                                
+
                                 <td>@{{usuario.username}}</td>
                                 <td>NOMBRE</td>
                                 <td>APELLIDO PATERNO</td>
                                 <td>APELLIDO PATERNO</td>
-                                
+
 
                               </tr>
-                            </tbody>  
+                            </tbody>
                       </table>
-                      
+
 
 
 
@@ -160,12 +160,12 @@
 
 
 
-                   
+
                   </div>
                 </div> <!-- fin del wizard container -->
               </div>
           </div> <!-- row -->
- 
+
 
  <!-- pruebas -->
          <div class="row">
@@ -187,7 +187,7 @@
 <script src="{{ asset('js/jquery-ui.js') }}"></script>
 <script src="{{ asset('js/wizard.js') }}"></script>
 <script type="text/javascript">
-    
+
 
 
 
@@ -196,43 +196,43 @@
             el: '#usuarioCRUD',
             //funcion al crear el objeto
             created:function(){
-                
+
                 this.getUsuarios();
                 //llamada al metodo
             },
 
 
             data:{
-                
+
                 errors:[],
                 usuarios:[],
                 searchUsuario:{'username':'','nombre':'','paterno':'','materno':''},
                     },
             methods:{
 
-    
+
                     getUsuarios:function () {
 
-                   
+
                     var url='/administrador/usuarios';
                     //variable que hace referencia a la URI de la ruta
                     axios.get(url).then(response=>{
                     this.usuarios=response.data;
-                    
+
                     });
-                    
+
                 },
 
                     busquedaUsuario:function()
                     {
                     var url='/administrador/usuario/search';
                     axios.post(url,{
-                        usuario:this.searchUsuario//new keep es e input 
+                        usuario:this.searchUsuario//new keep es e input
                     }).then(response=>{
                       this.usuarios=response.data;
                       this.errors=[];//listado de errores
                       this.searchUsuario={'username':'','nombre':'','paterno':'','materno':''}
-                        toastr.success('Busqueda exitosa');//mensaje 
+                        toastr.success('Busqueda exitosa');//mensaje
                        alert(response.data);
                     }).catch(error=>{
                       this.errors=error.response.data
@@ -242,12 +242,12 @@
                 },
 
 
-    
-                
-                
 
-                
-        
+
+
+
+
+
         }});
 </script>
 
